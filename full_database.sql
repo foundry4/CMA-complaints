b388fa5d9675b899e74d6294753b684eae88883a
mcoulthurst <mc@lightness.co.uk>
2020-03-27 11:30:52 +0000

    Rename report_other column

--- full_database.sql
+++ full_database.sql
@@ -14,7 +14,7 @@ ADD COLUMN report_business_pricing BOOLEAN DEFAULT FALSE,
 ADD COLUMN report_market_advantage BOOLEAN DEFAULT FALSE,
 ADD COLUMN report_misleading_claims BOOLEAN DEFAULT FALSE,
 ADD COLUMN report_cancellation BOOLEAN DEFAULT FALSE,
-ADD COLUMN report_reason_other VARCHAR (200),
+ADD COLUMN report_other VARCHAR (200),
 ADD created_at TIMESTAMP DEFAULT now();
 
 --To restart the sequence from a specified number (useful when changing between databases) use the following command
