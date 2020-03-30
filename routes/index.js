ce19e9a589268d239d05d165f7410dec95962472
Elizabeth Phillips <elizabethphillips@elizabeths-mbp.home>
2020-03-30 15:10:29 +0100

    validate products

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,34 @@ const { body, validationResult } = require('express-validator');
 const { formatValidationErrors } = require('../lib/utils');
 const save_to_cma_db = require('../lib/save_to_cma_db');
 const {reports,food_products,hygiene_products,medical_products} = require('../lib/constants');
+const products = [...food_products,...hygiene_products,...medical_products];
+
+
+const validate_pack_sizes =(body)=>{
+    const array = [];
+    for (index in products){
+        const name = products[index].name+'_pack_size';
+        array.push(body(name)
+            .if(body(name).notEmpty())
+            .isInt().withMessage('Enter a valid pack size for '+products[index].text));
+    }
+    return array;
+}
+
+const validate_expected_price =(body)=>{
+    const array = [];
+    for (index in products){
+        const current_name = products[index].name+'_current_price';
+        array.push(body(current_name)
+            .if(body(current_name).notEmpty())
+            .isNumeric().withMessage('Enter a valid curent price for '+products[index].text));
+        const expected_name = products[index].name+'_expected_price';
+        array.push(body(expected_name)
+            .if(body(expected_name).notEmpty())
+            .isNumeric().withMessage('Enter a valid expected price for '+products[index].text));
+    }
+    return array;
+}
 
 
 // GET home page
@@ -48,6 +76,41 @@ router.get('/which_products', function (req, res) {
         console.log('liz',err.toString());
     }
 });
+
+router.post('/which_products',
+    [ ...validate_pack_sizes(body),  ... validate_expected_price(body) ],
+    async (request, response) => {
+        try {
+            const errors = formatValidationErrors(validationResult(request))
+            if (!errors) {
+                console.log('no errors in validation');
+                request.session.data = {...request.session.data,...request.body};
+                response.redirect('/where_was_behaviour');
+            }
+            else {
+                let errorSummary = Object.values(errors);
+                console.log(errors);
+
+                console.log('found errors in validation');
+                try {
+                    response.render('which_products', {
+                        // reports,
+                        food_products,
+                        hygiene_products,
+                        medical_products,
+                        errors,
+                        errorSummary,
+                        values: request.body, // In production this should sanitized.
+                    });
+                } catch (err) {
+                    console.log('failed to render page', err.toString())
+                }
+            }
+        } catch (err) {
+            throw err.toString();
+        }
+    }
+);
 router.get('/what_is_business_url', function (req, res) {
     res.render('what_is_business_url', {});
 });
@@ -56,6 +119,7 @@ router.post('/where_was_behaviour', function (req, res) {
     res.redirect('where_was_behaviour');
 });
 router.get('/where_was_behaviour', function (req, res) {
+    console.log(req.session.data);
     res.render('where_was_behaviour', {});
 });
 router.get('/more_information', function (req, res) {
@@ -105,73 +169,6 @@ router.get('/privacy', function (req, res) {
 router.get('/confirm/:id',function (req, res) {
   res.render("confirm",{id:req.params.id});
 })
-
-router.post('/',
-    [   // Q1
-        body('report_reason')
-           .exists()
-           .not().isEmpty().withMessage('Enter the reason for your report'),
-        // Q2
-        body('company-location')
-            .exists()
-            .not().isEmpty().withMessage('Please indicate the location of the company the complaint is about.'),
-        // Q4
-        body('company-sector')
-            .exists()
-            .not().isEmpty().withMessage('Please indicate the sector of the company the complaint is about.'),
-        // Q5
-        body('product')
-            .exists()
-            .not().isEmpty().withMessage('Please give the name of the product.'),
-        // Q6
-        body('description')
-            .exists()
-            .not().isEmpty().withMessage('Please give a full description of the issue.'),
-        // Q9
-        body('evidence')
-            .exists()
-            .not().isEmpty().withMessage('Please indicate if there is evidence of the issue.'),
-        // Q10
-        body('contact-email').if(body('contact-email').notEmpty())
-            .isEmail().withMessage('Enter an email address in the correct format, like name@example.com'),
-    ],
-    async (request, response) => {
-        try {
-            request.session.data = {...request.session.data,...request.body};
-            const errors = formatValidationErrors(validationResult(request))
-            if (!errors) {
-                console.log('no errors in validation');
-                try {
-                    const ref = await save_to_cma_db(request.body,request);
-                    response.redirect('/confirm/'+ref);
-                }
-                catch (err){
-                    console.log('Failed to save to database',err.toString());
-                    response.render('error', { content : {error: {message: "Internal server error"}}});
-                }
-            }
-            else {
-                let errorSummary = Object.values(errors);
-                console.log(errors);
-                
-                console.log('found errors in validation');
-                try {
-                    response.render('index', {
-                        reports,
-                        errors,
-                        errorSummary,
-                        values: request.body, // In production this should sanitized.
-                    });
-                } catch (err) {
-                    console.log('failed to render page', err.toString())
-                }
-            }
-        } catch (err) {
-            throw err.toString();
-        }
-    }
-);
-
 router.get('/redirect', function (req, res, next) {
     res.render('redirect', {});
 });
