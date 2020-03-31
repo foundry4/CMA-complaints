fe64663ea7ad5f2fb0c27fe063af302b051d8ecc
Elizabeth Phillips <elizabethphillips@elizabeths-mbp.home>
2020-03-31 15:01:29 +0100

    ensure that one of product or other_product is selected

--- routes/index.js
+++ routes/index.js
@@ -237,7 +237,19 @@ router.get('/which_products', function (req, res) {
     }
 });
 router.post('/which_products',
-    [ ...validate_pack_sizes(body),  ... validate_expected_price(body) ],
+    [ ...validate_pack_sizes(body),  ... validate_expected_price(body),body(['product','other_product']).custom((value,{req}) => {
+        console.log('my value => ',req.body);
+        // const today = new Date();
+        // const today_year = today.getFullYear();
+        // // month is 0-11 in javascript so need to add to make it 1
+        // const today_month = today.getMonth()+1;
+        // const today_day = today.getDate();
+        // console.log('today = ', today_day, today_month, today_year)
+        // if (req.body['date-day']>today_day ||req.body['date-month']>today_month|| req.body['date-year']>today_year) {
+            throw new Error('Please select a product or provide details in the "Other product" category');
+        // }
+        // return true;
+    })  ],
     async (request, response) => {
         try {
             const errors = formatValidationErrors(validationResult(request))
@@ -248,7 +260,7 @@ router.post('/which_products',
             }
             else {
                 let errorSummary = Object.values(errors);
-                console.log(errors);
+                errorSummary = errorSummary.filter((a)=>a.id!=='product');
 
                 console.log('found errors in validation');
                 try {
@@ -450,7 +462,6 @@ router.post('/when_behaviour',
                 throw new Error('Please enter a date in the past');
             }
             return true;
-
         })],
     async (request, response) => {
         try {
